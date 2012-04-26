ae30c06564100ac6155212ba31bd7000b2a7b202
unknown <Bloodmaker@Bloodmaker-Lapt.(none)>
2012-04-26 15:23:18 +0200

    pas de modif

--- OngletAjoutSupprimer.java
+++ OngletAjoutSupprimer.java
@@ -1,6 +1,4 @@
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.JTextField;
+import javax.swing.*;
 import java.awt.*;
 
 
@@ -8,11 +6,16 @@ public class OngletAjoutSupprimer extends JPanel{
 	
 	public OngletAjoutSupprimer(){
 		
-		this.setLayout(new FlowLayout());		
-		JTextField tf1 = new JTextField(5); 
-		JButton but1 = new JButton("button 1");
+		this.setLayout(new FlowLayout());
+		JLabel label1 = new JLabel("Ajouter vos entrées ici");	
+		JTextField tff = new JTextField(15); 
+		JTextField tfe = new JTextField(15); 
+		JButton but1 = new JButton("Valider");
+		this.add(label1);
+		this.add(tff);
+		this.add(tfe);
 		this.add(but1);
-		this.add(tf1);
+		
 	}
 }
 
